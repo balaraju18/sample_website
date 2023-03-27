cb255e09cee6143eba463eb916f58fcc57e76038
balaraju <kambhampati.balaraj@gmail.com>
2023-03-27 14:10:55 +0530

    message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Misc from './components/pages/Misc'
+import ProductList from './components/pages/eCommerce';
+import Intranet from './components/pages/Intranet';
+import Crm from './components/pages/Crm'
+import Sidebar from './components/Sidebar';
+import News from './components/pages/News';
+import Listing from './components/pages/Listing'
+import Crypto from './components/pages/Crypto'
+import Chat from './components/pages/Chat';
+import Contact from './components/pages/Contact';
+import Mail from './components/pages/Mail';
+import Widgets from './components/pages/Widgets';
+import Metrics from './components/pages/Metrics';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Sidebar>
+        <Routes>
+          <Route path="/" element={<Misc />} />
+          <Route path="/misc" element={<Misc />} />
+          <Route path="/crypto" element={<Crypto />} />
+           <Route path="/listing" element={<Listing />} />
+          <Route path="/crm" element={<Crm />} />
+          <Route path="/intranet" element={<Intranet />} />
+          <Route path="/ecommerce" element={<ProductList />} />
+          <Route path="/news" element={<News />} />
+          <Route path="/chat" element={<Chat />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/mail" element={<Mail />} />
+          <Route path="/widgets" element={<Widgets />} />
+          <Route path="/metrics" element={<Metrics />} />
+          <Route path="/material Ui" element={<Metrics />} />
+        </Routes>
+      </Sidebar>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
